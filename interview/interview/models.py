a8b87b6493185a73c6f81d3c7c7dc1a7983328e5
Ashutosh Aswani <202051215@iiitvadodara.ac.in>
2022-12-17 10:03:41 +0530

    created models.py to show the schema of Users and Analysis Tables

---
+++ interview/interview/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+
+class Users(models.Model):
+    name = models.CharField(max_length=200)
+    email = models.CharField(max_length=200)
+    password = models.CharField(max_length=200)
+
+class Analysis(models.Model):
+    email = models.CharField(max_length=200,null=True)
+    questionID = models.IntegerField(null=True)
+    wpm = models.IntegerField(null=True)
+    fillerWords = models.CharField(max_length=500,null=True)
+    fillerPhrases = models.CharField(max_length=500,null=True)
